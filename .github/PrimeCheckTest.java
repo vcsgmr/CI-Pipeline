2a2a76a58d343512e913abc1a0575efa27f9a7d9
vcsgmr <102797885+vcsgmr@users.noreply.github.com>
2025-01-18 10:41:14 +0100

    Update PrimeCheckTest.java

--- .github/PrimeCheckTest.java
+++ .github/PrimeCheckTest.java
@@ -23,9 +23,9 @@ public class PrimeCheckTest {
   @Test
   public void testPrimes() {
     /* ToDo: insert a test here */
-    int[10] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    int[] prime = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
     for (int i = 0; i<10; i++){
-      assertEquals("this will fail", true, PrimeCheck.isPrime(prime[i]));
+      assertEquals("failed on prime " + i, true, PrimeCheck.isPrime(prime[i]));
     }
   }
 
@@ -33,9 +33,9 @@ public class PrimeCheckTest {
   @Test
   public void testNonPrimes() {
     /* ToDo: insert a test here */
-    int[10] nonprime = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18}
+    int[] nonPrime = {4, 6, 8, 9, 10, 12, 14, 15, 16, 18};
     for (int i = 0; i<10; i++){
-      assertEquals("this will fail", true, PrimeCheck.isPrime(nonprime[i]));
+      assertEquals("failed on non prime " + i, false, PrimeCheck.isPrime(nonPrime[i]));
     }
   }
 }
